75353363517460ba8a0e2eceadf3850c9e6ec63d
Nirjhar Agarwal <nirjhar.agarwal@gmail.com>
2023-09-28 23:41:25 -0400

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -28,8 +28,8 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train test split")
             train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of Data Completed")
 
@@ -38,7 +38,7 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e.sys)
+            raise CustomException(e,sys)
 
 if __name__ == "__main__":
     obj = DataIngestion()
--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 from src.logger import logging
 
@@ -5,9 +6,9 @@ def error_messages_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error Occured in python script name [{0}] line number [{1}] error messsage [{2}]".format(
-    file_name,exc_tb.tb_lineno,str(error)
+    file_name,exc_tb.tb_lineno,str(error))
     return error_message
-    )
+
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
